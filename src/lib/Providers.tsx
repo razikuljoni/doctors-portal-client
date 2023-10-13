1d20c7980cdf44b0ff8f60c63856192265fb599b
MD Razikul Islam Joni <razikul.joni69@gmail.com>
2023-10-13 18:31:42 +0600

    impliment axios and redux properly

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,10 +1,14 @@
+"use client";
+
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
 import StyledComponentsRegistry from "./AntdRegistry";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>
+        <Provider store={store}>
             <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-        </div>
+        </Provider>
     );
 };
 export default Providers;
